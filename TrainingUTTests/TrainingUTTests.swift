97bc502b4950933aa7a3d772b6ab0467dafc32d0
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2023-01-18 18:05:59 -0300

    Creating new project and testing right and wrong codes in XCTestCase clas

--- TrainingUTTests/TrainingUTTests.swift
+++ TrainingUTTests/TrainingUTTests.swift
@@ -12,10 +12,14 @@ final class TrainingUTTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        //This function is used to prepare the environment to start building the tests
+        print("Started")
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        //This function clean the tests variables created in setUpWithError
+        print("Finshed")
     }
 
     func testExample() throws {
@@ -24,13 +28,18 @@ final class TrainingUTTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        print("Testing rightExample")
+        XCTAssert(1+1 == 2)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testExamplo() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        print("Testing wrongExample")
+        XCTAssert(1+1 == 3)
     }
 
 }
