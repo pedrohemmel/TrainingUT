a6bfec53623c34b640d79b19f8f6bd2aeed72578
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2023-01-18 17:40:10 -0300

    Initial Commit

---
+++ TrainingUT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingUT
+//
+//  Created by Pedro henrique Dias hemmel de oliveira souza on 18/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
